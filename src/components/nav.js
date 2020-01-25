760e6957b66051ec1762d8a86f9099787256fc1f
kapilgupta101292 <kapilguptansit@gmail.com>
2020-01-25 16:37:21 +0530

    initial commit

---
+++ src/components/nav.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li className="logo">
+            Coding<span>Bits</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Mission</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
